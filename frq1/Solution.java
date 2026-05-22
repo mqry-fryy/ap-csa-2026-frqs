46269e02d5816610a07368eb8701dbc9a4b600d3
mqryy <marywu124@gmail.com>
2026-05-22 11:20:14 -0400

    Update Solution.java

--- frq1/Solution.java
+++ frq1/Solution.java
@@ -1,1 +1,35 @@
+// frq # 1
+public class Account {
+    private String username; // to be initialized in part(a)
 
+    public Account(String requestedName){   // a
+        if (isAvailable(requestedName)){
+            username = requestedName;
+        }
+        else 
+        {
+            int num = 1;
+            while (!isAvailable(requestedName + num)){
+                num++;
+            }
+            username = requestedName + num;
+        }
+    }
+
+    public static boolean isAvailable(String str){
+        return true;
+    }
+
+    public String getShortenedName(){// b
+        String shortened = "";
+        for(int i = 0; i < username.length(); i++){
+            if(username.charAt(i) == '-'){
+                shortened = shortened.substring(0,shortened.length()-1);
+            } else {
+                shortened += username.charAt(i);
+            }
+        }
+        return shortened;
+    }
+
+}
