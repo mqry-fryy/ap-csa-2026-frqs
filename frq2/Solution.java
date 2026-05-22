10d02055423fecaec35659e1f73513020440cb02
mqryy <marywu124@gmail.com>
2026-05-22 11:23:37 -0400

    Update Solution.java

--- frq2/Solution.java
+++ frq2/Solution.java
@@ -1,1 +1,17 @@
+public class Bottle {
+    private double currentAmount;
+    private double capacity;
 
+    public Bottle(double c) {
+        currentAmount = c;
+        capacity = c;
+    }
+
+    public double updateAmount(double amount) {
+        currentAmount -= amount;
+        if (currentAmount < 0.25*capacity) {
+            currentAmount = capacity;
+        }
+        return currentAmount;
+    }
+}
