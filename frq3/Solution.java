dc949f0c2e643350a94f63679bda7c63c9ce974c
mqryy <marywu124@gmail.com>
2026-05-28 18:17:35 -0400

    Update Solution.java

--- frq3/Solution.java
+++ frq3/Solution.java
@@ -1,1 +1,14 @@
+public class courseRecord{
+    int count = 0;
 
+    for (courseRecord h : historyList){
+        for (courseRecord m : mathList){
+            if (h.getStudentID().equals(m.getStudentID)) &&
+                h.getAbsences() > m.getAbsences())
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
