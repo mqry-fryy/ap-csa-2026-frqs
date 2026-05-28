e27623fd6b783199b5b9ccc9aba3b6afd362d80c
mqryy <marywu124@gmail.com>
2026-05-28 18:18:02 -0400

    Update Solution.java

--- frq4/Solution.java
+++ frq4/Solution.java
@@ -1,1 +1,18 @@
+public int getPointsForRow(int targetRow){
+    int sum = 0;
+    boolean sameColor = true;
 
+    String firstColor = board[targetRow][0].getColor();
+
+    for (int col = 0; col < board[targetRow].length; col++){
+        sum += board[targetRow][col].getPoints();
+
+        if (!board[targetRow][col].getColor().equals(firstColor)){
+            sameColor = false;
+        }
+    }
+    if (sameColor){
+        return sum*2;
+    }
+    return sum;
+}
